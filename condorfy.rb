bc1a00ceff7f9b3195a49c25f9f1914524c4b4cb
Sergio Botero <sergiobuj@gmail.com>
2012-05-24 10:55:50 -0500

    Render selected option from form ready to make it available for download
    Trimming parameters and need to remove nil or unspecified values/options.
    First checkpoint will be to create the basic [vanilla, parallel, standard]
    universe submission file.
    Added CSS for later tune up.

--- condorfy.rb
+++ condorfy.rb
@@ -5,14 +5,25 @@ before do
   content_type :html
 end
 
-get '/create' do
-  @binaries = `ls /usr/bin`.split(/\n/)
-  @max_num_jobs = 200
-  erb :form
+
+['/', '/create/?'].each do |req|
+  get req do
+    @binaries = `ls /usr/bin`.split(/\n/)
+    @max_num_jobs = 200
+    erb :form
+  end
 end
 
+post '/condorfy/?' do
+  params.keys.each do |key|
+    params[key].strip!
+    params.reject! {|key, value| value.nil? }
+  end
+  
+  params.keys.each do |key|
+    puts "-#{key}--#{params[key]}-"
+  end
 
-post '/condorfy' do
-  @sub_file = "bla bla bla"
+
   erb :condorfied
 end
