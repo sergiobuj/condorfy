e3952612f9ff6926690f55299ed36df7ac8931b2
Sergio Botero <sergiobuj@gmail.com>
2012-05-24 23:37:41 -0500

    Create condorfyle that holds the complete condorfied file
    Need to implement the download capability, both for the condor file
    and for the input file that is created with the textarea.
    Need to create textareas on the fly for the multimple input files
    (for parallel universe) and to set various environment variables.

--- condorfy.rb
+++ condorfy.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
-require 'sinatra/reloader'
+
+begin
+  require 'sinatra/reloader'
+rescue LoadError
+  # sinatra-contrib not installed
+end
 
 before do
   content_type :html
@@ -17,13 +22,34 @@ end
 post '/condorfy/?' do
   params.keys.each do |key|
     params[key].strip!
-    params.reject! {|key, value| value.nil? }
+  end
+  params.delete_if {|k, v| v.empty? }
+  
+  is_parallel = (params[:Universe] == "parallel")
+  
+  @condorfyle = "#### Auto generated Condor Submission file ####\n"
+  @condorfyle << "#### Sergio Botero sbotero2_a_eafit_edu_co ####\n\n\n"
+
+  ##### Create file #####
+  params.keys.each do | key |
+    if key == "jobs_or_cores"
+      if is_parallel
+        @condorfyle << "machine_count = #{params[key]}\n"
+      end
+    elsif key == "files_name"
+      @condorfyle << ((is_parallel)? "error = #{params[key]}.err.$(NODE)\n" : "error = #{params[key]}.err.$(PROCESS)\n")
+      @condorfyle << ((is_parallel)? "output = #{params[key]}.out.$(NODE)\n" : "output = #{params[key]}.out.$(PROCESS)\n")
+      @condorfyle << "log = #{params[key]}.log\n"
+    elsif key == "input_file_content"
+      # nil
+    else
+      @condorfyle << "#{key} = #{params[key]}\n"
+    end
+    
   end
   
-  params.keys.each do |key|
-    puts "-#{key}--#{params[key]}-"
-  end
-
-
+  @condorfyle << ((is_parallel)? "queue\n" : "queue #{params[:jobs_or_cores]}\n")  
+  
+  puts @condorfyle
   erb :condorfied
 end
